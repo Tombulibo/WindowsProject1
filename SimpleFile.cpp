dff07ba8886db34fd93f2eccf6589f1466eef349
Tombulibo <chinachengyuantong@163.com>
2021-09-29 16:37:15 +0800

    添加SimpleFile类

--- SimpleFile.cpp
+++ SimpleFile.cpp
@@ -1,1 +1,46 @@
 #include "SimpleFile.h"
+
+BOOLEAN SimpleFile::ReadFile(WCHAR* pathName)
+{
+	wifstream inFile(pathName, ios::in);
+	if (!inFile)
+	{
+		return FALSE;
+	}
+	inFile.imbue(locale(locale(), "", LC_CTYPE));
+	wstring buffer;
+	while (inFile.good())
+	{
+		std::getline(inFile, buffer);
+		FileContent.push_back(buffer);
+	}
+	inFile.close();
+
+	return TRUE;
+}
+
+BOOLEAN SimpleFile::WriteFile(WCHAR* pathName, WCHAR* fileLines)
+{
+	if (fileLines != NULL)
+	{
+		wstringstream mySS;
+		mySS << fileLines;
+		wofstream outFile(pathName, ios::out | ios::trunc);
+		if (!outFile)
+		{
+			return FALSE;
+		}
+		outFile.imbue(locale(locale(), "", LC_CTYPE));
+		wstring myString = mySS.str();
+		outFile.write(myString.c_str(), wcslen(fileLines));
+		outFile.close();
+	}
+
+	return TRUE;
+}
+
+vecLinesContainer SimpleFile::getFileLines(WCHAR* pathName)
+{
+	ReadFile(pathName);
+	return FileContent;
+}
--- SimpleFile.h
+++ SimpleFile.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <Windows.h>
+#include <FSTREAM>
+#include <VECTOR>
+#include <STRING>
+#include <sstream>
+using namespace std;
+
+typedef vector<wstring> vecLinesContainer;
+
 class SimpleFile
 {
 public:
 	SimpleFile();
 
+	BOOLEAN ReadFile(WCHAR* pathName);
+	BOOLEAN WriteFile(WCHAR* pathName, WCHAR* fileLines);
+
+	vecLinesContainer getFileLines(WCHAR* pathName);
+
+private:
+	vecLinesContainer FileContent;
 };
 
